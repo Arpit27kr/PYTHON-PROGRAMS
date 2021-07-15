f6d5628fc77cedbddd53795a4ba218a0442a5349
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-15 19:32:19 +0530

    Add files via upload

---
+++ prct111.py
@@ -0,0 +1,134 @@
+##capacity=20000
+##cost=3400
+##
+##n=int(input("Enter the capacity of power bank"))
+##c=int(input("Enter the cost of powerbank"))
+##
+##if n>10000 & c< 3500:
+##    print("'Yes, this power bank is for you!' if both the requirements are met")
+##else:
+##    print("No, this doesn't suit your needs.' if either condition is not met.")
+
+
+
+
+#elif
+
+##price=int(input("enter the price"))
+##qty=int(input("enter the quantity"))
+##amount=price*qty
+##if amount>5000:
+##    print("you got 5 prcnt discount")
+##    discount=amount*5/100
+##    amount=amount-discount
+##    print("total amount is ",amount)
+##elif amount>3000:
+##    print("you got 3 prcnt discount")
+##    discount=amount*3/100
+##    amount=amount-discount
+##else:
+##print("No discount")
+
+
+
+
+##use of while loop
+##
+##i=1
+##n=int(input("enter the number "))
+##while (i<10) :  #while with condition statement
+##    print(i*n) 
+##    i=i+1    #update
+##
+##
+###another method use of format key word
+##
+##i=1
+##while i < 13:
+##    print("16 * {} is {}" .format(i,16*i))
+##    i = i + 1
+
+##for x in range (0,5):
+##    print("iteration is ",x)
+
+###else with the while loop
+##x=0
+##while x<=5:
+##    x=x+1
+##    print("yes")
+##else
+
+
+
+
+
+
+
+
+
+
+##
+####program to cheack the number is prime or not
+##n=int(input("enter any number"))
+##count=0
+##
+##for x in range (2,n):
+##    if n%x==0:
+##        print("{} is not prime".format(n))
+##        break
+##    else:
+##        print("{} is prime".format(n))
+##        
+
+    
+
+###program to find prime factors
+##num=int(input("enter the number \n"))
+##d=2
+##while num>1:
+##    if num%2==0:
+##        print(d)
+##        num=num/d
+##        continue
+##    d=d+1
+
+for i in "arpit":
+  
+    continue
+     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

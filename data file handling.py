6db6244e6b3b3423abec38a0a4187965d64d6672
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-22 22:23:53 +0530

    Add files via upload

---
+++ data file handling.py
@@ -0,0 +1,7 @@
+f=open("arpit.txt" ,'w')
+f.write("I love python")
+f.write("\n my name is arpit I ;love to do python and its my first data file handling hope you will like it. ")
+print("sucessfully written")
+f.close()
+
+
